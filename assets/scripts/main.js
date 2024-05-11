c9a5af90830a84c126556fc9296645d31f0fddb7
matheusCarneiro <carneiromatheu@gmail.com>
2024-05-11 15:21:41 -0300

    feat: Adiciona envio de pedido

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -22,7 +22,7 @@ function deselect(id) {
 
 function enableCheckoutBtn() {
   const button = document.getElementById("checkout-btn");
-  
+
   button.removeAttribute("disabled");
   button.innerHTML = "Fechar pedido";
 }
@@ -41,4 +41,62 @@ function toggleSelection(element) {
   deselect(parentID);
   select(item);
   checkSelected();
+}
+
+function formatToNumber(string) {
+  const numericValue = parseFloat(string.replace("R$ ", "").replace(",", "."));
+  return numericValue;
+}
+
+function formatToString(num) {
+  const formattedValue = "R$ " + num.toFixed(2).replace(".", ",");
+  return formattedValue;
+}
+
+function sumPrices(...prices) {
+  const total = prices.reduce((acc, price) => acc + formatToNumber(price), 0);
+  return formatToString(total);
+}
+
+function getSelectedItemInfo(category) {
+  const item = getSelected(category);
+  const name = item.getElementsByClassName("name")[0].innerText;
+  const price = item.getElementsByClassName("price")[0].innerText;
+  return { name, price };
+}
+
+function getCustomerInfo() {
+  const customerName = prompt("Por favor, digite seu nome: ");
+  const customerAddress = prompt("Agora, por favor, digite seu endereço: ");
+  return { name: customerName, address: customerAddress };
+}
+
+function sendMessageToWpp(message) {
+  const number = "31993866415";
+  const text = encodeURIComponent(message);
+  const link = `https://wa.me//55${number}?text=${text}`;
+
+  window.open(link);
+}
+
+function orderCheckout() {
+  const dish = getSelectedItemInfo("dishes");
+  const drink = getSelectedItemInfo("drinks");
+  const dessert = getSelectedItemInfo("desserts");
+  const total = sumPrices(dish.price, drink.price, dessert.price);
+  const customerInfo = getCustomerInfo();
+  const orderMessage = `
+    Olá, gostaria de fazer o pedido:
+  
+    - Prato: ${dish.name},
+    - Bebida: ${drink.name},
+    - Sobremesa: ${dessert.name}
+    
+    Total: ${total}
+    
+    Nome: ${customerInfo.name}
+    Endereço: ${customerInfo.address}
+  `;
+
+  sendMessageToWpp(orderMessage);
 }
